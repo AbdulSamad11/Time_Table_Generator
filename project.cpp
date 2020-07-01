f474cb6be766c91ba6df4533078057bf36547306
ABDUL SAMAD <55202643+AbdulSamad11@users.noreply.github.com>
2020-07-01 12:52:25 +0500

    first commit to setup everything

---
+++ project.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int k=7; //total courses
+	
+	//can input for teachers courses classes later in this matrix
+	string avail[3][k]={
+		{"rubina","samyan","atif","amina","tauqeer","talia","laeeq"}, //list of teachers
+		{"mc"    ,"algo"  ,"dbms",  "os","taf",   "os_lab",  "dbms_lab"}, //list of courses
+		{"3"    ,"3"  ,"3",  "3","3",   "3",  "3"}, //x classes
+	
+	};
+	
+	return 0;
+}
